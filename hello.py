561c7092d6dbcdf8fbc1701804daa66eae50fc0f
QueenterJuma <jumahqueenter@gmail.com>
2024-03-16 15:02:07 +0300

    Add indentation

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 # import sys
 # print(sys.version)
 
-print("Hello, World!")
+# print("Hello, World!")
+
+#Python Indentation
+if 5>3:
+    print("Five is greater than three")
+if 4>2:
+    print("Four is also greater than two")
