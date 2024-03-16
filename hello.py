2e2c20fd9c26733c0c3b54ab31e4217b82b772dd
QueenterJuma <jumahqueenter@gmail.com>
2024-03-16 15:28:41 +0300

    Add comments

--- hello.py
+++ hello.py
@@ -4,7 +4,23 @@
 # print("Hello, World!")
 
 #Python Indentation
+#Indentation refers to the spaces at the beginning of a code line.
 if 5>3:
     print("Five is greater than three")
 if 4>2:
     print("Four is also greater than two")
+
+#Python Variables
+    #In python, variables are created when you assign a value to it:
+x = 12
+y = "Hello, there!"
+
+#COMMENTS
+# Single-line comment starts with '#' symbol
+"""
+Multiple lines can be commented using triple quotes '   """
+"""
+This is a multiple lines comments which spans across
+multiple lines and uses three double quotes on each side
+of the text.
+"""
