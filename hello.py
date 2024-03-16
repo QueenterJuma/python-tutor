fd0a5070ce86d496022a637dea43683baff931ab
QueenterJuma <jumahqueenter@gmail.com>
2024-03-16 16:26:21 +0300

    Commit message:
    "Added detailed explanations and context to Python variables, types, and arithmetic operators."

--- hello.py
+++ hello.py
@@ -11,9 +11,51 @@ if 4>2:
     print("Four is also greater than two")
 
 #Python Variables
-    #In python, variables are created when you assign a value to it:
+    """
+Variables are containers for storing data values
+They must be created before they can be used in
+an expression or statement. Variable names cannot start with
+a number and can contain letters (A-Z
+and a-z), digits (0-9
+and underscore _). There are several types of
+variables available in Python like local variables, global variables
+and built-in variable.
+"""
+x = 10           #Integer
+y = "I am a string"   #
+print(type(y))     #To check
+#the type of a variable we use
+#The type() function returns the type of an
+#object along with its value.
+#Arithmetic Operators
+num1 = 10+5      #
+num2 = num1 - 7   #
+product = num1 * num2   #
+quotient = num1 / num2   #
+remainder = num1 % num2
+print(f"{num1} + {
+    5} = {num1 + 5
+          }")
+print(f"{num1} - {
+    7} = {num2
+          }")
+print(f"{num1} * {
+        num2} = {product}")
+print(f"{num1} / {
+    num2} = { quotient}")
+print(f"{num1} % {
+    num2} = { remainder}")
+#Assignment operators
+num3 = 10
+num4 = 3
+num3 += 5       #Adds and
+#assigns the result to num3
+"""
+In python, variables are created when you first assign a value to it:
+
 x = 12
 y = "Hello, there!"
+"""
 
 #COMMENTS
 # Single-line comment starts with '#' symbol
