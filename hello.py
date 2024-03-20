2c2ca87863835eaf2599155ef1cf807d3e1f12a4
QueenterJuma <jumahqueenter@gmail.com>
2024-03-20 07:07:50 +0300

    Add random numbers

--- hello.py
+++ hello.py
@@ -136,3 +136,14 @@ written with j instead of i. For example,
 4 + 3*j.
 
 """
+#Random Number
+"""Python does not have a random() function to make a random number, 
+but Python has a built-in module called random that can be used to
+make random numbers:
+
+Example
+Import the random module, and display a random number between 1 and 9:
+
+import random
+
+print(random.randrange(1, 10))"""
