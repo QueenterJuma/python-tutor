5341ab82411c916368f461ac2202a05f3df9bc87
QueenterJuma <jumahqueenter@gmail.com>
2024-03-21 22:44:49 +0300

    Add casting convertion

--- hello.py
+++ hello.py
@@ -153,3 +153,21 @@ print(random.randrange(1, 10))"""
 and complex() functions to cast values from one type
 to another. The str() function can also be
 used to convert a value to a string."""
+
+#ExampleGet your own Python Server
+
+#Integers:
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#Floats:
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+#Strings:
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
