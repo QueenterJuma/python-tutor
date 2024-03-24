615d55efc4787d4f8fa0f4458d452b356202bff5
QueenterJuma <jumahqueenter@gmail.com>
2024-03-24 23:47:14 +0300

    Add if not in

--- hello.py
+++ hello.py
@@ -261,4 +261,14 @@ Check if "expensive" is NOT present in the following text:
 txt = "The best things in life are free!"
 print("expensive" not in txt) 
 Output: True
+
+***Use it in an if statement:
+
+Example
+print only if "expensive" is NOT present:
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+  Output: No, 'expensive' is NOT
 '''
