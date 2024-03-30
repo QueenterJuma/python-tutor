a23faac8d892df83bc9c889efd41dce2265ad279
QueenterJuma <jumahqueenter@gmail.com>
2024-03-30 10:15:46 +0300

    Add Python slicing strings

--- hello.py
+++ hello.py
@@ -393,3 +393,21 @@ if "expensive" not in txt:
 print("No,'expensive' is NOT present.")
 Output: No,'expensive' is NOT present
 '''
+#Pyyhon -Slicing Strings
+#Slicing
+"""
+You can return a range of characters by using the slice syntax.
+
+Specify the start index and the end index, separated by a colon, 
+to return a part of the string.
+
+ExampleGet your own Python Server
+Get the characters from position 2 to position 5 (not included):
+
+b = "Hello, World!"
+print(b[2:5])
+Output: llo
+
+***NOTE// The first character has index 0.
+"""
+
