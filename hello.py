067d947a552f4c207a8454e9fbeafc8452e68ac8
QueenterJuma <jumahqueenter@gmail.com>
2024-03-30 10:28:36 +0300

    Add Slice to the end

--- hello.py
+++ hello.py
@@ -417,4 +417,15 @@ b =  "Hello, World!"
 print(b[:5])
 Output: Hello
 '''
+#Slice To the End
+'''
+By leaving out the end index, the range will go to the end:
+
+Example:
+Get the characters from position 2, and all the way to the end:
+
+b = "Hello, World!"
+print(b[2:])
+Output: llo, World!
+'''
 
