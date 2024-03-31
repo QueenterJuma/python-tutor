2de3d1eb8decc4e1997eee2f6718d155083be6f4
QueenterJuma <jumahqueenter@gmail.com>
2024-03-31 19:31:48 +0300

    Add negative indexing

--- hello.py
+++ hello.py
@@ -429,3 +429,18 @@ print(b[2:])
 Output: llo, World!
 '''
 
+#Negative Indexing
+'''
+Use negative indexes to start the  slicing from the end  of the string:
+Example:
+Example
+Get the characters:
+
+From: "o" in "World!" (position -5)
+
+To, but not included: "d" in "World!" (position -2):
+
+b = "Hello, World!"
+print(b[-5:-2])
+Output: orl
+'''
