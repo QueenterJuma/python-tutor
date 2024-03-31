290cc51d842d858fe2425d3df5a08dba0ff3470a
QueenterJuma <jumahqueenter@gmail.com>
2024-03-31 22:58:50 +0300

    Add format

--- my_str.py
+++ my_str.py
@@ -336,3 +336,48 @@ c = a + " " + b
 print(c)
 # Output: Hello, World!
 
+#Python - Format-strings
+
+#String Format
+'''we cannot combine strings and numbers like this:
+'''
+#Example
+age = 26
+txt = "My name is Queen, I am" + age
+print(txt)
+
+'''
+But we can combine strings and numbers by using the format() method!
+
+The format() method takes the passed arguments, 
+formats them, and places them in the string 
+where the placeholders {} are:
+'''
+#Example
+'''Use the format() method to insert numbers 
+into strings'''
+
+age = 26
+txt = "My  name is Queen, I am {}".
+print(txt.format(age)) 
+#Output: My  name is Queen, I am 26
+
+'The format() method takes unlimited number of arguments, and are placed into the respective placeholders:'
+
+#Example
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+"You can use index numbers {0} to be sure the arguments are placed in the correct placeholders:"
+#***(Note that Python uses zero based counting)
+
+#Example
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+#output: I want to pay 49.95 dollars for 3 pieces of item 567
